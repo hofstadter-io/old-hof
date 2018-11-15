832b1ae9b97e9cbd5e53b89c17a22cfa579573de
Tony Worm <verdverm@gmail.com>
2018-11-15 13:11:57 -0700

    make separate funcs

--- lib/sync/archive.go
+++ lib/sync/archive.go
@@ -32,23 +32,13 @@ func Untar(dst string, r io.Reader) error {
 	}
 
 
-	var R io.ReadCloser
-
 	gzr, err := gzip.NewReader(r)
 	if err != nil {
-		fmt.Println("Trying ZLib")
 
-		z, err := zlib.NewReader(r)
-		if err != nil {
-			return err
-		}
-		R = z
-	} else {
-		R = gzr
 	}
-	defer R.Close()
+	defer gzr.Close()
 
-	tr := tar.NewReader(R)
+	tr := tar.NewReader(gzr)
 
 	for {
 		header, err := tr.Next()
@@ -188,3 +178,85 @@ func Tar(src string, writers ...io.Writer) error {
 
 	return nil
 }
+
+// Untar takes a destination path and a reader; a tar reader loops over the tarfile
+// creating the file structure at 'dst' along the way, and writing any files
+func Unzlib(dst string, r io.Reader) error {
+
+	for _, dir := range avail {
+		err := os.RemoveAll(dir)
+		if err != nil {
+			return err
+		}
+	}
+
+	z, err := zlib.NewReader(r)
+	if err != nil {
+		return err
+	}
+	defer z.Close()
+
+	tr := tar.NewReader(z)
+
+	for {
+		header, err := tr.Next()
+
+		switch {
+
+		// if no more files are found return
+		case err == io.EOF:
+			return nil
+
+		// return any other error
+		case err != nil:
+			return err
+
+		// if the header is nil, just skip it (not sure how this happens)
+		case header == nil:
+			continue
+		}
+
+		// the target location where the dir/file should be created
+		target := filepath.Join(dst, header.Name)
+
+		// the following switch could also be done using fi.Mode(), not sure if there
+		// a benefit of using one vs. the other.
+		// fi := header.FileInfo()
+
+		// check the file type
+		switch header.Typeflag {
+
+		// if its a dir and it doesn't exist create it
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
+
+		// if it's a file create it
+		case tar.TypeReg:
+			dir := filepath.Dir(target)
+			fmt.Println("x ", target)
+			if _, err := os.Stat(dir); err != nil {
+				if err := os.MkdirAll(dir, 0755); err != nil {
+					return err
+				}
+			}
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+
+			// copy over contents
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
+
+			// manually close here after each file operation; defering would cause each file close
+			// to wait until all operations have completed.
+			f.Close()
+		}
+	}
+}
+
